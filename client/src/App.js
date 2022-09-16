c7da6b142f3364577892614966d93df47b942b03
velopace <velopace@outlook.com>
2022-09-16 16:27:42 -0500

    bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -19,12 +19,12 @@ const App = () => {
     return (
         <Container maxWidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography class={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img class={classes.image} src={memories} alt="memories" height="60" />
+                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
                             <Posts />
                         </Grid>
