3d8ec166ed2287b104db70fed4903b4878ddc042
velopace <velopace@outlook.com>
2022-09-17 10:18:10 -0500

    create post form

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@gql-velopace.tvw7q.mongodb.net/?retryWrites=true&w=majority`;
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@gql-velopace.tvw7q.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
