db5678801a1ea985a273788a863a52be87b5ef08
velopace <velopace@outlook.com>
2022-09-18 14:32:26 -0500

    edit/update post

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -6,7 +6,7 @@ import Post from './Post/Post';
 
 import useStyles from './styles';
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
     const posts = useSelector((state) => state.posts);
     const classes = useStyles();
 
@@ -15,7 +15,7 @@ const Posts = () => {
             <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}>
                 {posts.map((post) => (
                     <Grid item key={post._id} xs={12} sm={6}>
-                        <Post post={post} />
+                        <Post post={post} setCurrentId={setCurrentId} />
                     </Grid>
                 ))}
             </Grid>
