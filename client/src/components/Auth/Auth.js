192633370e8f2e0eaa6071bacbbc7d60ad8418d5
velopace <velopace@outlook.com>
2022-09-19 22:43:05 -0500

    google auth

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,14 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Avatar, Button, Container, Grid, Paper, Typography } from "@material-ui/core";
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { GoogleLogin } from 'react-google-login';
+import { gapi } from 'gapi-script';
+import { useDispatch } from "react-redux";
+import { useNavigate } from 'react-router-dom';
 
 import useStyles from './styles';
 import Input from "./Input";
+import Icon from "./icon";
+import { AUTH } from "../../constants/actionTypes";
 
 const Auth = () => {
     const classes = useStyles();
     const [showPassword, setShowPassword] = useState(false);
     const [isSignup, setIsSignup] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const googleClientId = '917897438345-tacbut6sesmsrm7b1euga7oms227kfof.apps.googleusercontent.com';
+
+    useEffect(() => {
+        const initClient = () => {
+            gapi.client.init({
+                clientId: googleClientId,
+                scope: ''
+            });
+        };
+        gapi.load('client:auth2', initClient);
+    }, [])
 
     const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
 
@@ -22,6 +42,24 @@ const Auth = () => {
 
     const switchMode = () => {
         setIsSignup((prevIsSignup) => !prevIsSignup);
+    }
+
+    const googleSuccess = async (res) => {
+        const result = res?.profileObj;
+        const token = res?.tokenId;
+
+        try {
+            dispatch({ type: AUTH, data: { result, token } });
+
+            navigate('/');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const googleError = (error) => {
+        console.log(error);
+        console.log('Google Sign In was unsuccessful. Try Again Later');
     }
 
     return (
@@ -42,11 +80,22 @@ const Auth = () => {
                         <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
                         <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
                         { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" /> }
-                        <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
-                            { isSignup ? 'Sign Up' : 'Sign In' }
-                        </Button>
                     </Grid>
-                    <Grid container justify="flex-end">
+                    <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                        { isSignup ? 'Sign Up' : 'Sign In' }
+                    </Button>
+                    <GoogleLogin
+                        clientId={googleClientId}
+                        render={(renderProps) => (
+                        <Button className={classes.googleButton} color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained">
+                            Google Sign In
+                        </Button>
+                        )}
+                        onSuccess={googleSuccess}
+                        onFailure={googleError}
+                        cookiePolicy="single_host_origin"
+                    />
+                    <Grid container justifyContent="flex-end">
                         <Grid item>
                         <Button onClick={switchMode}>
                             { isSignup ? 'Already have an account? Sign in' : "Don't have an account? Sign Up" }
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { LOGOUT } from "../../constants/actionTypes";
 
 import useStyles from './styles';
 import memories from '../../images/memories.png';
 
 const Navbar = () => {
     const classes = useStyles();
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const location = useLocation();
 
-    const user = null;
+    const logout = () => {
+        dispatch({ type: LOGOUT });
+        navigate('/');
+        setUser(null);
+    }
+
+    useEffect(() => {
+        const token = user?.token;
+
+        setUser(JSON.parse(localStorage.getItem('profile')));
+    }, [location]);
 
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
@@ -21,7 +37,7 @@ const Navbar = () => {
                     <div className={classes.profile}>
                         <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name}</Avatar>
                         <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-                        <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+                        <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
                     </div>
                 ): (
                     <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
