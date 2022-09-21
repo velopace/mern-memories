4f51d411b242cbd10f8b05aa0d50f57cb954e51c
velopace <velopace@outlook.com>
2022-09-20 19:54:10 -0500

    update app with auth

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { useDispatch } from "react-redux";
-import { LOGOUT } from "../../constants/actionTypes";
+import decode from 'jwt-decode';
 
+import { LOGOUT } from "../../constants/actionTypes";
 import useStyles from './styles';
 import memories from '../../images/memories.png';
 
@@ -22,6 +23,12 @@ const Navbar = () => {
 
     useEffect(() => {
         const token = user?.token;
+
+        if (token) {
+            const decodedToken = decode(token);
+
+            if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+        }
 
         setUser(JSON.parse(localStorage.getItem('profile')));
     }, [location]);
