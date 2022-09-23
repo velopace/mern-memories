a54c39a4ae9354d07032a37fd88fdd33b1f8db62
velopace <velopace@outlook.com>
2022-09-23 15:35:26 -0500

    update logo

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -6,7 +6,8 @@ import decode from 'jwt-decode';
 
 import { LOGOUT } from "../../constants/actionTypes";
 import useStyles from './styles';
-import memories from '../../images/memories.png';
+import memoriesLogo from '../../images/memories-Logo.png';
+import memoriesText from '../../images/memories-Text.png';
 
 const Navbar = () => {
     const classes = useStyles();
@@ -35,10 +36,10 @@ const Navbar = () => {
 
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
-            <div className={classes.brandContainer}>
-                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60" />
-            </div>
+            <Link to="/" className={classes.brandContainer}>
+                <img src={memoriesText} alt="icon" height="45px" />
+                <img className={classes.image} src={memoriesLogo} alt="icon" height="40px" />
+            </Link>
             <Toolbar className={classes.toolbar}>
                 {user ? (
                     <div className={classes.profile}>
